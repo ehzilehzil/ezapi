78c3c5d0224e0abc046d9681d7a5f88ac3879077
ehzilehzil <ehzilehzil@outlook.com>
2025-11-16 09:51:11 +0900

    cors fixed

--- main.ts
+++ main.ts
@@ -1,10 +1,20 @@
 import { Hono } from "jsr:@hono/hono@4.10.5";
+import { cors } from "jsr:@hono/hono@4.10.5/cors"
 import { zValidator } from "jsr:@hono/zod-validator@0.7.4";
 import { test } from "./routes/test.ts";
 import { mtsp } from "./routes/mtsp.ts";
 
 
 const app = new Hono();
+
+app.use("*", cors({
+  origin: [
+    "http://localhost:4321",
+    "https://ehzilehzil.github.io"
+  ],
+  allowMethods: ["GET", "POST", "OPTIONS"],
+  allowHeaders: ["Content-Type", "Authorization"],
+}));
 
 app.post("/test", zValidator("json", test.schema), async (c) => {
   const data = c.req.valid("json");
